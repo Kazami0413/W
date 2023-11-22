2264340bf27b40275d287726e2cfc0f08d3d836d
XiaXia009 <wli61412@gmail.com>
2023-11-22 22:07:07 +0800

    new

--- main.py
+++ main.py
@@ -11,10 +11,6 @@ data ={
     "password": password
 }
 
-@app.route("/")
-def home():
-    return render_template("index.html")
-
 @app.route("/login")
 def login():
     email = request.args.get("email")
@@ -48,7 +44,32 @@ def singin():
 
     with open(JSON_FILE, 'w') as using_json_file:
         json.dump(existing_data, using_json_file, indent=2)
-    
 
-if __name__ == "__main__":
-    app.run
+@app.route('/singup', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        print(name, email, password)
+        data ={
+        "email": email,
+        "password": password,
+        "user": name
+        }
+        try:
+            with open(JSON_FILE, 'r') as using_json_file:
+                existing_data = json.load(using_json_file)
+        except FileNotFoundError:
+            existing_data = []
+
+        existing_data.append(data)
+
+        with open(JSON_FILE, 'w') as using_json_file:
+            json.dump(existing_data, using_json_file, indent=2)
+        return render_template('index.html')
+    else:
+        return render_template('singup.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
